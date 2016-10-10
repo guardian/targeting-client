e9e159f643824cc67b10ff484419fa9e88789391
npapacostas <nick.papacostas@guardian.co.uk>
2016-10-10 08:30:11 -0700

    Adding badge campaign types

--- src/test/scala/com/gu/targeting/client/CampaignTest.scala
+++ src/test/scala/com/gu/targeting/client/CampaignTest.scala
@@ -5,16 +5,21 @@ import play.api.libs.json._
 import org.scalatest.{FreeSpec, Matchers}
 
 class CampaignTests extends FreeSpec with Matchers {
-  val expectedFields = EmailFields("testName", "testTheme", "testAbout", "testDescription", "testFrequency", "testListId")
-
   val requiredTags = List("should/exist", "also/exists")
   val lackingTags = List("not/this", "also/not-this")
   val rules = List(Rule(requiredTags, lackingTags))
 
   val id = UUID.randomUUID()
 
-  "Campaign should convert to JSON correctly" in {
-    val campaign = Campaign(id, "name", rules, 10, None, None, false, expectedFields)
-    Json.toJson(campaign).toString
+  "Campaign should convert email campaigns to JSON correctly" in {
+    val emailFields = EmailFields("testName", "testTheme", "testAbout", "testDescription", "testFrequency", "testListId")
+    val campaign = Campaign(id, "name", rules, 10, None, None, false, emailFields)
+    Campaign.fromJson(Json.toJson(campaign)) should equal(campaign)
+  }
+
+  "Campaign should convert badge campaigns to JSON correctly" in {
+    val badgeFields = BadgeFields("testTag", "badgeUrl", None)
+    val campaign = Campaign(id, "name", rules, 10, None, None, false, badgeFields)
+    Campaign.fromJson(Json.toJson(campaign)) should equal(campaign)
   }
 }

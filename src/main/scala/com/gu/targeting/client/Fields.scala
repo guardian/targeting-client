8715420ba0d4533a3157352c7c5941540459e646
Nicholas Papacostas <NickPapacostas@users.noreply.github.com>
2016-10-11 13:45:07 +0100

    Badges (#12)
    * Adding badge campaign types
    
    * Added badgeFields to getFieldType method

--- src/main/scala/com/gu/targeting/client/Fields.scala
+++ src/main/scala/com/gu/targeting/client/Fields.scala
@@ -6,6 +6,7 @@ import play.api.libs.functional.syntax._
 trait Fields
 
 case class EmailFields(name: String, theme: String, about: String, description: String, frequency: String, listId: String) extends Fields
+case class BadgeFields(seriesTag: String, imageUrl: String, classModifier: Option[String]) extends Fields
 
 // Add more fields here as applicable
 
@@ -14,8 +15,15 @@ object Fields {
   val reservedTypeField = "_type"
 
   val emailType = "email"
+  val badgeType = "badge"
 
-  val allTypes = List(emailType)
+  val allTypes = List(emailType, badgeType)
+
+  val badgeFormat = (
+      (JsPath \ "seriesTag").format[String] and
+      (JsPath \ "imageUrl").format[String] and
+      (JsPath \ "classModifier").formatNullable[String]
+    )(BadgeFields.apply, unlift(BadgeFields.unapply))
 
   val emailFormat = (
       (JsPath \ "name").format[String] and
@@ -30,6 +38,7 @@ object Fields {
     override def writes(field: Fields): JsValue = {
       field match {
         case f: EmailFields => emailFormat.writes(f) + (reservedTypeField, JsString(emailType))
+        case f: BadgeFields => badgeFormat.writes(f) + (reservedTypeField, JsString(badgeType))
         case other => {
           throw new UnsupportedOperationException(s"Unable to serialize field of type ${other.getClass}")
         }
@@ -41,6 +50,7 @@ object Fields {
     override def reads(json: JsValue): JsResult[Fields] = {
       (json \ reservedTypeField).get match {
         case JsString(`emailType`) => emailFormat.reads(json)
+        case JsString(`badgeType`) => badgeFormat.reads(json)
         case other => JsError(s"Unexpected step type value: $other")
       }
     }

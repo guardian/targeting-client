e0e86241d15e689290ad0d533dd4091d2ce8c8cf
Sam Desborough <sam.desborough@gmail.com>
2016-12-14 11:26:28 +0000

    Add matchAllTags flag

--- src/main/scala/com/gu/targeting/client/Rule.scala
+++ src/main/scala/com/gu/targeting/client/Rule.scala
@@ -1,19 +1,20 @@
 package com.gu.targeting.client
 
-import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
-case class Rule(requiredTags: List[String], lackingTags: List[String])
+case class Rule(requiredTags: List[String], lackingTags: List[String], matchAllTags: Boolean)
 
 object Rule {
   implicit val ruleFormat = (
     (JsPath \ "requiredTags").format[List[String]] and
-    (JsPath \ "lackingTags").format[List[String]]
-  )(Rule.apply, unlift(Rule.unapply))
+      (JsPath \ "lackingTags").format[List[String]] and
+      (JsPath \ "matchAllTags").format[Boolean]
+    )(Rule.apply, unlift(Rule.unapply))
 
   def evaluate(rule: Rule, tags: Seq[String]): Boolean = {
-    tags.intersect(rule.requiredTags).nonEmpty &&
-      tags.intersect(rule.lackingTags).isEmpty
+    if (rule.matchAllTags) tags.intersect(rule.lackingTags).isEmpty
+    else tags.intersect(rule.requiredTags).nonEmpty && tags.intersect(rule.lackingTags).isEmpty
   }
 
 }

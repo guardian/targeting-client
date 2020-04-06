b68806717882130f0d16eab546a747636af5c5e9
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-04-06 15:10:23 +0100

    Bump _everything_

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,14 +1,12 @@
 import sbt._
 
 object Dependencies {
-  val awsSdkVersion = "1.11.26"
-  val dynamodb = "com.amazonaws" % "aws-java-sdk-dynamodb" % awsSdkVersion
+  val dynamodb = "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.11.759"
 
   val commonsIo = "org.apache.commons" % "commons-io" % "1.3.2"
-  val scalatic = "org.scalactic" %% "scalactic" % "3.0.4"
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4" % "test"
-  val http = "org.apache.httpcomponents" % "httpclient" % "4.3.4"
+  val scalatic = "org.scalactic" %% "scalactic" % "3.1.1"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1" % "test"
+  val http = "org.apache.httpcomponents" % "httpclient" % "4.5.12"
 
-  val playJson25 = "com.typesafe.play" %% "play-json" % "2.5.18"
-  val playJson26 = "com.typesafe.play" %% "play-json" % "2.6.7"
+  val playJson26 = "com.typesafe.play" %% "play-json" % "2.7.4"
 }

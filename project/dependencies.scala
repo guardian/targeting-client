123361808cf15a3012757e76bdb9a6d9cb5d72fa
Ioanna Kokkini <ioannakok@hotmail.com>
2023-12-15 17:37:27 +0000

    Support multiple Play json versions concurrently
    These are the consumers of this library:
    * frontend: Scala 2.13, Play 2.8 & 3.0
    * MAPI: Scala 2.13, Play 2.8
    * targeting: Scala 2.13, Play 2.8
    
    We now want to upgrade frontend to Play 3: https://github.com/guardian/frontend/pull/26755 but we also need to ensures that versions of this library are available to consumers that are stil on Play 2.8.
    
    The updated sbt configuration is based on techniques used in https://github.com/guardian/facia-scala-client and https://github.com/guardian/play-googleauth where it's been successful for a few years now.
    
    Co-authored-by: Roberto Tyley <roberto.tyley@guardian.co.uk>

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -5,5 +5,18 @@ object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1" % Test
   val http = "org.apache.httpcomponents" % "httpclient" % "4.5.12"
 
-  val playJson = "com.typesafe.play" %% "play-json" % "2.8.1"
+  case class PlayJsonVersion(
+    majorMinorVersion: String,
+    groupId: String,
+    exactPlayJsonVersion: String
+  ) {
+    val projectId = s"client-play-json-v$majorMinorVersion"
+
+    val lib: ModuleID = groupId %% "play-json" % exactPlayJsonVersion
+  }
+
+  object PlayJsonVersion {
+    val V28 = PlayJsonVersion("28", "com.typesafe.play", "2.8.2")
+    val V30 = PlayJsonVersion("30", "org.playframework", "3.0.1")
+  }
 }

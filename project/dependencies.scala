c7711b6b5e35b284d955f46add85d981c5482110
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2026-01-08 14:19:51 +0000

    Bump jackson version on a per-version basis

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -13,9 +13,22 @@ object Dependencies {
     val projectId = s"client-play-json-v$majorMinorVersion"
 
     val lib: ModuleID = groupId %% "play-json" % exactPlayJsonVersion
+
+    val jacksonVersion = "2.20.1"
+    val jacksonDatabindVersion = "2.20.1"
+
+    val overrides: Seq[ModuleID] = Seq(
+      "com.fasterxml.jackson.core" % "jackson-core" % jacksonVersion,
+      "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % jacksonVersion,
+      "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % jacksonVersion,
+      "com.fasterxml.jackson.dataformat" % "jackson-dataformat-cbor" % jacksonVersion,
+      "com.fasterxml.jackson.module" % "jackson-module-parameter-names" % jacksonVersion,
+      "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion,
+      "com.fasterxml.jackson.core" % "jackson-databind" % jacksonDatabindVersion
+    )
   }
 
   object PlayJsonVersion {
-    val V30 = PlayJsonVersion("30", "org.playframework", "3.0.10")
+    val V30 = PlayJsonVersion("30", "org.playframework", "3.0.6")
   }
 }
